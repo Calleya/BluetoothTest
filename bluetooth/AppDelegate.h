5aa181b56f0ac7996d26f6fa8723dcda78620f64
袁春燕 <597288735@qq.com>
2016-10-17 14:13:10 +0800

    Initial Commit

---
+++ bluetooth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bluetooth
+//
+//  Created by spk on 16/10/17.
+//  Copyright © 2016年 com.i-spk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bluetooth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bluetooth
+//
+//  Created by spk on 16/10/17.
+//  Copyright © 2016年 com.i-spk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
